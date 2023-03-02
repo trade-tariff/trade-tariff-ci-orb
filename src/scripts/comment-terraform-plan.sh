d410bf1490e6ca4efbc5617569a5ad44f71fe39c
Amber Cronin <amber@amber.vision>
2023-03-02 12:12:39 +0000

    [BAU] Apply missed fixes to violations of SC2086

--- src/scripts/comment-terraform-plan.sh
+++ src/scripts/comment-terraform-plan.sh
@@ -1,17 +1,17 @@
 PARAM_GITHUB_USER=$(eval echo "\$$GITHUB_USER_ENV_VAR_NAME")
 PARAM_GITHUB_TOKEN=$(eval echo "\$$GITHUB_TOKEN_ENV_VAR_NAME")
 
-if [ "${PARAM_GITHUB_TOKEN}" = "" ];then
+if [ "${PARAM_GITHUB_TOKEN}" = "" ]; then
   echo "GITHUB_TOKEN not set."
   exit 1
 fi
 
-if [ "${PARAM_GITHUB_USER}" = "" ];then
+if [ "${PARAM_GITHUB_USER}" = "" ]; then
   echo "GITHUB_USER not set."
   exit 1
 fi
 
-if [ "${PARAM_PLAN}" = "" ];then
+if [ "${PARAM_PLAN}" = "" ]; then
   echo "No terraform plan output provided."
   exit 1
 fi
@@ -19,7 +19,7 @@ fi
 PR_RESPONSE=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
 -u "$PARAM_GITHUB_USER":"$PARAM_GITHUB_TOKEN")
 
-if [ "$(echo ${PR_RESPONSE} | jq length)" -eq 0 ]; then
+if [ "$(echo "${PR_RESPONSE}" | jq length)" -eq 0 ]; then
   echo "No PR found to update, skipping."
   # Exit 0 to not error and stop the workflow.
   exit 0
@@ -27,7 +27,7 @@ fi
 
 COMMENT=$(jo body="\`\`\`\n$(terraform show -no-color "${PARAM_PLAN}")")
 
-PR_COMMENT_URL=$(echo ${PR_RESPONSE} | jq --raw-output ".[]._links.comments.href")
+PR_COMMENT_URL=$(echo "${PR_RESPONSE}" | jq --raw-output ".[]._links.comments.href")
 
 curl --location --request POST "${PR_COMMENT_URL}" \
 -u "${PARAM_GITHUB_USER}":"${PARAM_GITHUB_TOKEN}" \
