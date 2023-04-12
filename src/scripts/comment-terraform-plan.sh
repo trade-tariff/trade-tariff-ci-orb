6c9a7403d170cd7299886c3864fbcadf31382781
Amber Cronin <amber@amber.vision>
2023-04-12 15:33:38 +0100

    BAU: Add hide-expired-comments command, job

--- src/scripts/comment-terraform-plan.sh
+++ src/scripts/comment-terraform-plan.sh
@@ -1,37 +1,49 @@
-PARAM_GITHUB_USER=$(eval echo "\$$GITHUB_USER_ENV_VAR_NAME")
-PARAM_GITHUB_TOKEN=$(eval echo "\$$GITHUB_TOKEN_ENV_VAR_NAME")
+#!/bin/sh
 
-if [ "${PARAM_GITHUB_TOKEN}" = "" ]; then
+environment=$1
+
+param_github_user=$(eval echo "\$$GITHUB_USER_ENV_VAR_NAME")
+param_github_token=$(eval echo "\$$GITHUB_TOKEN_ENV_VAR_NAME")
+terraform_plan=$(eval echo "\$$PARAM_PLAN")
+
+if [ "${param_github_token}" = "" ]; then
   echo "GITHUB_TOKEN not set."
   exit 1
 fi
 
-if [ "${PARAM_GITHUB_USER}" = "" ]; then
+if [ "${param_github_user}" = "" ]; then
   echo "GITHUB_USER not set."
   exit 1
 fi
 
-if [ "${PARAM_PLAN}" = "" ]; then
+if [ "${terraform_plan}" = "" ]; then
   echo "No terraform plan output provided."
   exit 1
 fi
 
-PR_RESPONSE=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
--u "$PARAM_GITHUB_USER":"$PARAM_GITHUB_TOKEN")
+pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
+-u "$param_github_user":"$param_github_token")
 
-if [ "$(echo "${PR_RESPONSE}" | jq length)" -eq 0 ]; then
+if [ "$(echo "${pr_response}" | jq length)" -eq 0 ]; then
   echo "No PR found to update, skipping."
   # Exit 0 to not error and stop the workflow.
   exit 0
 fi
 
-COMMENT=$(jo body="\`\`\`\n$(terraform show -no-color "${PARAM_PLAN}")")
+post_plan_comment() {
+  pr_comment_url="$1"
+  plan_output="$2"
 
-PR_COMMENT_URL=$(echo "${PR_RESPONSE}" | jq --raw-output ".[]._links.comments.href")
+  message=$(jo body="$(printf "**Terraform plan output for $environment:**\n\`\`\`\n%s" "$plan_output")")
 
-curl --location --request POST "${PR_COMMENT_URL}" \
--u "${PARAM_GITHUB_USER}":"${PARAM_GITHUB_TOKEN}" \
---header 'Content-Type: application/json' \
---data-raw "${COMMENT}"
+  curl --silent --location --request POST "$pr_comment_url" \
+  -u "$GITHUB_USER":"$GITHUB_TOKEN" \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/vnd.github+json' \
+  --data "$message"
+}
 
-exit 0
+plan_output=$(jo body="\`\`\`\n$(terraform show -no-color "${terraform_plan}")")
+pr_comment_url=$(echo "${pr_response}" | jq --raw-output ".[]._links.comments.href")
+
+post_plan_comment "$pr_comment_url" "$plan_output"
---
+++ src/scripts/hide-expired-comments.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+param_github_user=$(eval echo "\$$GITHUB_USER_ENV_VAR_NAME")
+param_github_token=$(eval echo "\$$GITHUB_TOKEN_ENV_VAR_NAME")
+
+if [ "${param_github_token}" = "" ]; then
+  echo "GITHUB_TOKEN not set."
+  exit 1
+fi
+
+if [ "${param_github_user}" = "" ]; then
+  echo "GITHUB_USER not set."
+  exit 1
+fi
+
+fetch_pr_number() {
+  local pr_response
+  local pr_number
+
+  pr_response=$(curl --silent --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
+        -u "$param_github_user":"$param_github_token")
+  pr_number=$(echo "$pr_response" | jq -r ".[0].number")
+
+  echo "$pr_number"
+}
+
+applicable_comment_ids() {
+  local comments
+
+  comments=$(fetch_comments)
+
+  echo "$comments" | jq ".[] | select(.author.login == \"$param_github_user\") | select(.isMinimized == false) | .id"
+}
+
+fetch_comments() {
+  local query
+  local gql_response
+
+  query=$(jo query="$(gql_query_for_comment)")
+  pr_response=$(curl --silent --location --request POST "https://api.github.com/graphql" \
+      -u "$param_github_user":"$param_github_token" \
+      --header 'Content-Type: application/json' \
+      --header 'Accept: application/vnd.github+json' \
+      --data "$(echo "$query" | jq -c .)")
+
+  echo "$gql_response" | jq -r ".data.repository.pullRequest.comments.nodes"
+}
+
+gql_query_for_comment() {
+  local pr_number
+  local fetch_comments_query
+
+  pr_number=$(fetch_pr_number)
+  fetch_comments_query="query {
+    repository(owner: \"${CIRCLE_PROJECT_USERNAME}\", name: \"${CIRCLE_PROJECT_REPONAME}\") {
+      pullRequest(number: ${pr_number}) {
+        comments(first: 100) {
+          nodes {
+            id
+            body
+            author {
+              login
+            }
+            createdAt
+            isMinimized
+            minimizedReason
+          }
+        }
+      }
+    }
+  }"
+
+  echo "$fetch_comments_query"
+}
+
+gql_mutation_for_comment() {
+  local node_id
+  local query
+
+  node_id=$1
+
+  # The enum value for OUTDATED is not configured correctly in the GraphQL schema (this is a bug
+  # in the GitHub API). The value is 'outdated' rather than 'OUTDATED'
+  query="mutation {
+    minimizeComment(input: {subjectId: $node_id, classifier: OUTDATED}) {
+      minimizedComment {
+        isMinimized
+      }
+    }
+  }"
+
+  echo "$query"
+}
+
+mark_previous_comments_as_expired() {
+  local comment_ids
+  local query
+  local response
+
+  comment_ids=$(applicable_comment_ids)
+
+  if [ -z "$comment_ids" ]; then
+    echo "No applicable comments found"
+  else
+    echo "Minimizing comments..."
+
+    echo "Comment IDs:"
+    echo "$comment_ids"
+
+    for node_id in $comment_ids; do
+      query=$(jo query="$(gql_mutation_for_comment "$node_id")")
+      response=$(curl --silent --location --request POST "https://api.github.com/graphql" \
+        -u "$param_github_user":"$param_github_token" \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/vnd.github+json' \
+        --data "$(echo "$query" | jq -c .)")
+
+      echo "Response:"
+      echo "$response"
+    done
+  fi
+}
+
+mark_previous_comments_as_expired
