9a6d7d2e77d8949abb709d428c8c93b41e83b986
Amber Cronin <amber@amber.vision>
2023-07-04 16:02:43 +0100

    HOTT-3514: Address comment about healthchecking

--- src/scripts/healthcheck.sh
+++ src/scripts/healthcheck.sh
@@ -3,6 +3,7 @@
 cf_app=$(eval echo "\$$PARAM_CF_APP")
 space=$(eval echo "\$$PARAM_SPACE")
 env_key=$(eval echo "\$$PARAM_ENV_KEY")
+path=$(eval echo "\$$HEALTHCHECK_PATH")
 
 dark_app="$cf_app-$env_key-dark"
 
@@ -21,29 +22,17 @@ if [ "${env_key}" = "" ]; then
   exit 1
 fi
 
-# Verify healthcheck endpoint on new app
-healthcheck_response=$(curl -s -o /dev/null -w "%{http_code}" \
-  "https://$dark_app.london.cloudapps.digital/healthcheck")
-
-if [ "$healthcheck_response" -ne 200 ]; then
-  echo "dark route not available, failing deploy ($healthcheck_response)"
-  cf logs "$dark_app" --recent
-  cf delete -f "$dark_app"
+if [ "${path}" = "" ]; then
+  echo "healthcheck_path parameter not set."
   exit 1
 fi
 
-if [ "$space" == "development" ] && [ "${CYPRESS_DEVELOPMENT_BASIC_AUTH}" == "true" ]; then
-  BASIC_AUTH="-u ${CYPRESS_DEVELOPMENT_BASIC_USERNAME}:${CYPRESS_DEVELOPMENT_BASIC_PASSWORD}"
-elif [ "$space" == "staging" ] && [ "${CYPRESS_STAGING_BASIC_AUTH}" == "true" ]; then
-  BASIC_AUTH="-u ${CYPRESS_STAGING_BASIC_USERNAME}:${CYPRESS_STAGING_BASIC_PASSWORD}"
-fi
+# Verify healthcheck endpoint on new app
+healthcheck_response=$(curl -s -o /dev/null -w "%{http_code}" \
+  "https://$dark_app.london.cloudapps.digital/${path}")
 
-healthcheck_response=$(curl -s -o /dev/null "${BASIC_AUTH}" \
-  -w "%{http_code}" \
-  "https://$dark_app.london.cloudapps.digital/find_commodity")
-
-if [ "$healthcheck_response" -ne 200 ];then
-  echo "dark app home page not available, failing deploy ($healthcheck_response)"
+if [ "$healthcheck_response" -ne 200 ]; then
+  echo "dark route not available, failing deploy ($healthcheck_response)"
   cf logs "$dark_app" --recent
   cf delete -f "$dark_app"
   exit 1
