ec900ae2423592a1caf36c1159ee7eb95e4c21d0
Amber Cronin <amber@amber.vision>
2023-07-04 12:42:25 +0100

    HOTT-3514: Convert healthcheck to command

--- src/scripts/healthcheck.sh
+++ src/scripts/healthcheck.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
-app=$1
-environment=$2
-environment_key=$3
+cf_app=$(eval echo "\$$PARAM_CF_APP")
+space=$(eval echo "\$$PARAM_SPACE")
+env_key=$(eval echo "\$$PARAM_ENV_KEY")
 
-cf_app="$app-$environment_key-dark"
+dark_app="$cf_app-$env_key-dark"
 
-# Verify healthcheck endpoint on new app
-healthcheck_response=$(curl -s -o /dev/null -w "%{http_code}" \
-  "https://$cf_app.london.cloudapps.digital/healthcheck")
-
-if [ "$healthcheck_response" -ne 200 ]; then
-  echo "dark route not available, failing deploy ($healthcheck_response)"
-  cf logs "$cf_app" --recent
-  cf delete -f  "$cf_app"
+if [ "${cf_app}" = "" ]; then
+  echo "cf_app parameter not set."
   exit 1
 fi
 
-if [ "$environment" == "development" ] && [ "${CYPRESS_DEVELOPMENT_BASIC_AUTH}" == "true" ]; then
+if [ "${space}" = "" ]; then
+  echo "space parameter not set."
+  exit 1
+fi
+
+if [ "${env_key}" = "" ]; then
+  echo "environment_key parameter not set."
+  exit 1
+fi
+
+# Verify healthcheck endpoint on new app
+healthcheck_response=$(curl -s -o /dev/null -w "%{http_code}" \
+  "https://$dark_app.london.cloudapps.digital/healthcheck")
+
+if [ "$healthcheck_response" -ne 200 ]; then
+  echo "dark route not available, failing deploy ($healthcheck_response)"
+  cf logs "$dark_app" --recent
+  cf delete -f "$dark_app"
+  exit 1
+fi
+
+if [ "$space" == "development" ] && [ "${CYPRESS_DEVELOPMENT_BASIC_AUTH}" == "true" ]; then
   BASIC_AUTH="-u ${CYPRESS_DEVELOPMENT_BASIC_USERNAME}:${CYPRESS_DEVELOPMENT_BASIC_PASSWORD}"
-elif [ "$environment" == "staging" ] && [ "${CYPRESS_STAGING_BASIC_AUTH}" == "true" ]; then
+elif [ "$space" == "staging" ] && [ "${CYPRESS_STAGING_BASIC_AUTH}" == "true" ]; then
   BASIC_AUTH="-u ${CYPRESS_STAGING_BASIC_USERNAME}:${CYPRESS_STAGING_BASIC_PASSWORD}"
 fi
 
 healthcheck_response=$(curl -s -o /dev/null "${BASIC_AUTH}" \
   -w "%{http_code}" \
-  "https://$cf_app.london.cloudapps.digital/find_commodity")
+  "https://$dark_app.london.cloudapps.digital/find_commodity")
 
 if [ "$healthcheck_response" -ne 200 ];then
   echo "dark app home page not available, failing deploy ($healthcheck_response)"
-  cf logs "$cf_app" --recent
-  cf delete -f "$cf_app"
+  cf logs "$dark_app" --recent
+  cf delete -f "$dark_app"
   exit 1
 fi
