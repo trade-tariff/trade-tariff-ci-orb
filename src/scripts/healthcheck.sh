2bf11c8f674f7f6cf062adeaef9e608f8a6409a3
William Fish <william.michael.fish@gmail.com>
2023-07-20 13:48:18 +0100

    [semver:patch] BAU: Parameterise domain when testing and pivoting app

--- src/scripts/healthcheck.sh
+++ src/scripts/healthcheck.sh
@@ -4,7 +4,7 @@ dark_app="${PARAM_CF_APP}-${PARAM_ENV_KEY}-dark"
 
 # Verify healthcheck endpoint on new app
 healthcheck_response=$(curl -s -o /dev/null -w "%{http_code}" \
-  "https://$dark_app.london.cloudapps.digital/${HEALTHCHECK_PATH}")
+  "https://$dark_app.${DOMAIN}/${HEALTHCHECK_PATH}")
 
 if [ "$healthcheck_response" -ne 200 ]; then
   echo "dark route not available, failing deploy ($healthcheck_response)"
