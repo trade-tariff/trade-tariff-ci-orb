6171acbc3529dbb87f825739e7752f22f5973725
Amber Cronin <amber@amber.vision>
2023-12-11 14:09:58 +0000

    HOTT-4598: Apply fixes from ShellCheck

--- src/scripts/tag.sh
+++ src/scripts/tag.sh
@@ -2,9 +2,6 @@
 
 docker_tag=$(git rev-parse --short HEAD)
 
-RELEASE_VERSION=$(< RELEASE_FILE)
-echo "RELEASE_VERSION is '${RELEASE_VERSION}'"
-
 if [ -z "${ECR_REPO}" ]; then
   echo "Missing ECR_REPO"
   exit 1
@@ -15,10 +12,13 @@ if [ -z "${RELEASE_VERSION}" ]; then
   exit 1
 fi
 
+RELEASE_VERSION=$(< RELEASE_FILE)
+echo "RELEASE_VERSION is '${RELEASE_VERSION}'"
+
 DOCKER_IMAGE="${ECR_REPO}/<< parameters.image-name >>"
 IMAGE_TAG="release-${RELEASE_VERSION}"
 
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ECR_REPO
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin "$ECR_REPO"
 docker pull "${DOCKER_IMAGE}:${docker_tag}"
 docker tag "${DOCKER_IMAGE}:${docker_tag}" "${DOCKER_IMAGE}:${IMAGE_TAG}"
 docker push "${DOCKER_IMAGE}:${IMAGE_TAG}"
